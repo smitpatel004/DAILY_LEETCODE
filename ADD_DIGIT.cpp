6732ffd718fd1b7d1a0f70027519fdb58b788f27
Smit Patel <155005588+smitpatel004@users.noreply.github.com>
2025-10-10 22:25:12 +0530

    Create ADD_DIGIT.cpp

---
+++ ADD_DIGIT.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+ while(num>=10){
+  int sum=0;
+  while(num!=0){
+            int r = num %10;
+            sum =r+sum;
+             num = num/10;
+        }
+        num =sum;   
+}
+return num;
+    }
+};
