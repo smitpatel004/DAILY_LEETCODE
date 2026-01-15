cc0ce9c80e447734dc8c5fc73ab329c03c134e52
Smit Patel <155005588+smitpatel004@users.noreply.github.com>
2026-01-15 23:50:52 +0530

    Create  recoverOrder.cpp

---
+++ recoverOrder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        vector<int>ans;
+        for(int i=0;i<order.size();i++){
+            for(int j=0;j<friends.size();j++){
+                if(order[i]==friends[j]){
+                    ans.push_back(order[i]);
+                }
+            }
+        }
+        // sort(ans.begin(),ans.end());
+        // reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
